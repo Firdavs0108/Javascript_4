87c7a7c07d1dce6c5a8ca834ca25bf7f4c79569e
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-03-13 20:28:52 +0900

    first commit

---
+++ task1.js
@@ -0,0 +1,87 @@
+  
+
+  let i = 0
+  
+
+  while(i<=10) {
+    
+      console.log(i);
+    i++
+  }
+
+
+// for(let i = 1; i<=10; i++) {
+//     if (i % 2 !== 0) { 
+
+//         continue
+//     }
+//     console.log(i);
+// }
+
+
+//=================1
+
+// let a = 10
+// let S = a ** 2
+
+// console.log(S);
+
+
+
+//===================2
+
+
+
+// let a = 5
+// let b = 8
+
+// let S = a * b 
+
+// let P = 2 * (a+b)
+
+// console.log(S)
+// console.log(P)
+
+//======================3
+
+
+// let d = 50
+// let l = 3.14
+
+// let T = l * d
+
+// console.log(T);
+
+//======================4
+
+
+// let a = 6
+
+// let V = a ** 3
+
+// let S = 6 * a ** 2
+
+// console.log(V);
+// console.log(S);
+
+//======================5
+
+// let a = 4
+// let b = 6
+// let c = 8
+
+// let V = a * b * c
+
+// let S = 2 * (a*b + b*c + a*c)
+
+// console.log(V);
+// console.log(S);
+
+
+
+
+
+
+
+
+
---
+++ task2.js
@@ -0,0 +1,73 @@
+
+
+//======================6
+
+
+// let L = 3.14
+// let R = 40
+
+// let T = 2 * L * R 
+// let S = L * R ** 2
+
+// console.log( Number. parseInt(T) );
+// console.log(R);
+
+//======================7
+
+// let a = 55
+// let b = 20
+
+// let = (a+b) / 2
+
+// console.log( Number. parseInt (let));
+
+
+//======================8
+
+// let a = 64
+// let b = 81
+
+// let= a ** (1/2) * b ** (1/2)
+
+// console.log(let);
+
+//======================9
+
+let a = 5
+let b = 7
+
+let1 = a+b
+let2 = a*b
+let3 = a**2
+let4 = b**2
+
+console.log(let1);
+console.log(let2);
+console.log(let3);
+console.log(let4);
+
+
+//======================10
+
+
+// let a = 10
+// let b = 15
+
+// let1 = a+b
+// let2 = a*b
+
+
+// console.log(let1);
+// console.log(let2);
+
+
+//======================11
+
+// let a = 8
+// let b = 9
+
+// let c = a**2 + b**2 
+// let p = a+b+c
+
+// console.log(c);
+// console.log(p);
